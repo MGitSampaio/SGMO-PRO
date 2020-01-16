7edad315cc1927b49b5deb00f085bedcb7adf676
Marcelo Sampaio <marcelosampaio@msn.com>
2020-01-16 12:09:26 -0300

    Alteração na label da versão

--- app/src/main/java/com/marcelosampaio/sgmo_pro/dao/CargoDao.java
+++ app/src/main/java/com/marcelosampaio/sgmo_pro/dao/CargoDao.java
@@ -106,7 +106,7 @@ public class CargoDao
 
     private void inserirCargo() {
         ContentValues values = new ContentValues();
-        values.put("status","Vigilante");
+        values.put("cargos","Vigilante");
         banco.insert("status",null,values);
         banco.close();
     }
